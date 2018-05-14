c91599b05ca90feb63bf86cbfa8cc3354f62144b
884292610 <884292610@hs-03784-macair.ad.edenpr.org>
2018-05-14 09:04:24 -0500

    game

---
+++ src/bingo/Game.java
@@ -0,0 +1,56 @@
+package bingo;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Game {
+	
+	ArrayList<Integer> guesses; 
+	int [] [] board = new int [5][5];
+	int guess;
+	
+	public Game () {
+		
+		for (int row = 0; row < board.length; row++) {
+			
+			for (int col = 0; col < board[0].length; col++) {
+				
+				board [row][col] = makeRandom();
+				
+			}
+		}
+			
+		
+	}
+	
+	public void print() {
+		
+		System.out.println("|-----------------------------------------------|");
+		System.out.println("|\tB.\tI.\tN.\tG.\tO.\t|");
+		System.out.println("|-----------------------------------------------|");
+	
+		
+		for (int row = 0; row < board.length; row++) {
+			
+			for (int col = 0; col < board[0].length; col++) {
+				
+				if (col != 4) {
+					
+					System.out.print("\t" + board [row][col]);
+					
+				} else {
+					
+					System.out.print("\t" + board [row][col] + "\n");
+					
+				}
+			}
+		}
+		
+	}
+	
+	public static int makeRandom() {
+		Random r = new Random();
+		return r.nextInt(99)+1;
+	}
+
+}
--- src/bingo/Runner.java
+++ src/bingo/Runner.java
@@ -5,8 +5,9 @@ public class Runner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello");
-
+		Game game = new Game();
+		game.print();
 	}
+
 
 }
