25cad59ef294484ed37ed9b88e82b11c070b53dd
90303947 <90303947@hs-06023-macair.ad.edenpr.org>
2018-05-22 09:42:37 -0500

    with button

---
+++ src/bingo/Input.java
@@ -0,0 +1,24 @@
+package bingo;
+
+public class Input {
+	
+	int num;
+
+	String correct;
+	
+	public Input (int num) {
+		this.num = num;
+		this.correct = "x";
+	}
+	
+	public String getCorrect() {
+		return correct;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+	
+	for (int row = 0; row )
+	
+}
